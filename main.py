cf8656e04ac997adad5e423bf4a1211cd403c852
Akihiro OKAYAMA <akihiroxob@gmail.com>
2024-03-22 12:42:54 +0900

    it works!

--- main.py
+++ main.py
@@ -2,113 +2,133 @@
 # -*- coding:utf-8 -*-
 import sys
 import os
-picdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pics')
-libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'libs')
-print(libdir);
 
+picdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'pics')
+fontdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'fonts')
+libdir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'libs')
 if os.path.exists(libdir):
     sys.path.append(libdir)
 
 import logging
 from waveshare_epd import epd7in3f
 import time
+import calendar
+import locale
 import datetime
+import random
+import requests
 from PIL import Image,ImageDraw,ImageFont,ImageEnhance
 import traceback
 
 logging.basicConfig(level=logging.DEBUG)
-
-def getDayLeft():
+def get_day_left():
     target_date = datetime.datetime(year=2024, month=10, day=23, hour=0)
     diff = target_date - datetime.datetime.now()
     return diff.days
 
-def scale(image: Image, target_width=480, target_height=800) -> Image:
-    """
-    Given an Pillow image and the two dimensions scale it, 
-    cropping centrally if required.
-    """
-    width, height = image.size
-    if height/width < target_height/target_width:
-        warn('too wide: cropping')
-        new_height = target_height
-        new_width = int(width * new_height / height)
+def get_date(datetime):
+    date = ['月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日', '日曜日']
+    return date[datetime.weekday()]
+
+def resize_image(img, size):
+    if (img.height > img.width):
+        height = round(img.height * size / img.width)
+        return img.resize((size, height))
     else:
-        warn('too tall: cropping')
-        new_width = target_width
-        new_height = int(height * new_width / width)
-    print(height, width, target_height, target_width, new_height, new_width)
-    # Image.ANTIALIAS is depracated --> Image.Resampling.LANCZOS
-    # but a fresh install of pillow via ``ARCHFLAGS='-arch arm6' python3 -m pip install pillow``
-    # yielded 8.1.2 as of 26/02/23
-    ANTIALIAS = Image.Resampling.LANCZOS if hasattr(Image, 'Resampling') else Image.ANTIALIAS
-    img = image.resize((new_width, new_height), ANTIALIAS)
-    # (left, top, right, bottom)
-    half_width_delta = (new_width - target_width) // 2
-    half_height_delta = (new_height - target_height) // 2
-    img = img.crop((half_width_delta, half_height_delta,
-                    half_width_delta + target_width, half_height_delta + target_height
-                   ))
-    return img
+        width = round(img.width * size / img.height);
+        return img.resize((width, size))
+
+def enhance_image(img):
+    enhanced_image: Image = ImageEnhance.Color(img).enhance(3)
+    pal_image = Image.new('P', (1,1))
+    pal_image.putpalette( (0,0,0,  255,255,255,  0,255,0,   0,0,255,  255,0,0,  255,255,0, 255,128,0) + (0,0,0)*249)
+    return enhanced_image.convert('RGB').quantize(palette=pal_image)
+
+def get_today_forecast():
+    url = 'https://api.weatherapi.com/v1/forecast.json';
+    query = {
+            'q': 'tokyo',
+            'days': 1,
+            'aqi': 'no',
+            'alerts': 'no',
+            'key': os.environ['WEATHER_APPID']
+    }
+    response = requests.get(url, params = query);
+    if response.status_code == 200:
+        data = response.json()
+        return data['forecast']['forecastday'][0]['day']
+    else:
+        return False
 
 try:
-    logging.info("epd7in3f Demo")
+    logging.info('start drawing on epd7in3f')
 
     epd = epd7in3f.EPD()
-    logging.info("init and Clear")
+    logging.info('init and Clear')
     epd.init()
     epd.Clear()
-    font18 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 18)
-    font24 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 24)
-    font40 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 40)
-    font80 = ImageFont.truetype(os.path.join(picdir, 'Font.ttc'), 80)
-    
-#    # Drawing on the image
-#    logging.info("1.Drawing on the image...")
-#    Himage = Image.new('RGB', (epd.width, epd.height), epd.WHITE)  # 255: clear the frame
-#    draw = ImageDraw.Draw(Himage)
-#    draw.text((5, 0), 'hello world', font = font18, fill = epd.RED)
-#    draw.text((5, 20), '7.3inch e-Paper (F)', font = font24, fill = epd.YELLOW)
-#    draw.text((5, 45), u'微雪电子', font = font40, fill = epd.GREEN)
-#    draw.text((5, 85), u'微雪电子', font = font40, fill = epd.BLUE)
-#    draw.text((5, 125), u'微雪电子', font = font40, fill = epd.ORANGE)
-#
-#    draw.line((5, 170, 80, 245), fill = epd.BLUE)
-#    draw.line((80, 170, 5, 245), fill = epd.ORANGE)
-#    draw.rectangle((5, 170, 80, 245), outline = epd.BLACK)
-#    draw.rectangle((90, 170, 165, 245), fill = epd.GREEN)
-#    draw.arc((5, 250, 80, 325), 0, 360, fill = epd.RED)
-#    draw.chord((90, 250, 165, 325), 0, 360, fill = epd.YELLOW)
-#    epd.display(epd.getbuffer(Himage))
-#    time.sleep(3)
 
-    # read bmp file 
-    logging.info("2.read bmp file")
-    original_image = Image.open(os.path.join(picdir, '1001.001.jpeg'))
+    # setup
+    font16 = ImageFont.truetype(os.path.join(fontdir, 'Noto_Sans_JP/NotoSansJP-Regular.ttf'), 16)
+    font20 = ImageFont.truetype(os.path.join(fontdir, 'Noto_Sans_JP/NotoSansJP-Bold.ttf'), 20)
+    font28 = ImageFont.truetype(os.path.join(fontdir, 'Noto_Sans_JP/NotoSansJP-Bold.ttf'), 28)
+    font32 = ImageFont.truetype(os.path.join(fontdir, 'Noto_Sans_JP/NotoSansJP-Bold.ttf'), 32)
+    font40 = ImageFont.truetype(os.path.join(fontdir, 'Noto_Sans_JP/NotoSansJP-Bold.ttf'), 40)
+    font68 = ImageFont.truetype(os.path.join(fontdir, 'Noto_Sans_JP/NotoSansJP-Bold.ttf'), 68)
+    font80 = ImageFont.truetype(os.path.join(fontdir, 'Noto_Sans_JP/NotoSansJP-Bold.ttf'), 80)
+    font100 = ImageFont.truetype(os.path.join(fontdir, 'Noto_Sans_JP/NotoSansJP-Bold.ttf'), 100)
 
-    enhanced_image: Image = ImageEnhance.Color(original_image).enhance(3)
-    pal_image = Image.new("P", (1,1))
-    pal_image.putpalette( (0,0,0,  255,255,255,  0,255,0,   0,0,255,  255,0,0,  255,255,0, 255,128,0) + (0,0,0)*249)
-    endithered: Image = enhanced_image.convert("RGB").quantize(palette=pal_image)
+    # create base image
+    logging.info('start to create image')
+    background = Image.new('RGB', (epd.width, epd.height), epd.WHITE)  # 255: clear the frame
 
-    draw = ImageDraw.Draw(endithered)
-    draw.rectangle((0, 520, 480, 800), fill = epd.WHITE)
-    draw.text((160, 560), 'あと', font = font40, fill = epd.YELLOW)
-    draw.text((160, 600), str(getDayLeft()), font = font80, fill = epd.WHITE)
-    draw.text((300, 640), 'にち', font = font40, fill = epd.YELLOW)
-    epd.display(epd.getbuffer(endithered))
+    # paste left side
+    i = random.randrange(58) + 1;
+    original_image = Image.open(os.path.join(picdir, f'picts.{i}.jpg'));
+    resized_image = resize_image(original_image, 480);
+    enhanced_image = enhance_image(resized_image);
+    pos_left = round((480 - enhanced_image.width) / 2);
+    pos_top = round((480 - enhanced_image.height) / 2);
+    background.paste(enhanced_image, (pos_left, pos_top));
+
+    # create right side
+    cover = Image.new('RGB', (320, 480), epd.WHITE)
+    draw = ImageDraw.Draw(cover)
+
+    today = datetime.datetime.now();
+    draw.text((124, 32), str(today.year), font = font28, fill = epd.BLACK)
+    draw.text((124, 64), f'令和{today.year - 2018}年', font = font20, fill = epd.BLACK)
+
+    draw.text((92, 112), f'{today.month:02}月{today.day}日', font = font32, fill = epd.BLACK)
+    draw.text((120, 148), get_date(today), font = font28, fill = epd.BLACK)
+
+    ## wright weather info if get from weather api
+    result = get_today_forecast()
+    if result != False:
+        weather_image = Image.open(os.path.join(picdir, f'weather/sunny@2x.png')); 
+        cover.paste(weather_image, (60, 192))
+        draw.text((94,196), f'{result["maxtemp_c"]}/{result["mintemp_c"]}℃', font=font16, fill = epd.BLACK)
+        pop_image = Image.open(os.path.join(picdir, f'weather/humidity@2x.png')); 
+        cover.paste(pop_image, (196, 192))
+        draw.text((230,196), f'{result["avghumidity"]:02}%', font = font16, fill = epd.BLACK)
+
+    draw.text((50, 296), '洗足学園小学校', font = font32, fill = epd.BLACK)
+    draw.text((98, 338), '試験まで', font = font32, fill = epd.BLACK)
+    draw.text((60, 368), str(get_day_left()), font = font80, fill = epd.BLACK)
+    draw.text((212, 378), '日', font = font68, fill = epd.BLACK)
+    background.paste(cover, (480, 0))
+
+    # draw to epaper
+    epd.display(epd.getbuffer(background))
     time.sleep(3)
 
-    #logging.info("Clear...")
-    #epd.Clear()
-    
-    logging.info("Goto Sleep...")
+    logging.info('Goto Sleep...')
     epd.sleep()
-        
+
 except IOError as e:
     logging.info(e)
-    
-except KeyboardInterrupt:    
-    logging.info("ctrl + c:")
+
+except KeyboardInterrupt:
+    logging.info('ctrl + c:')
     epd7in3f.epdconfig.module_exit(cleanup=True)
     exit()
